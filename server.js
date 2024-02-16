4b77edf39ef8ee5516e18fb7f80749f8645726c5
jimhunterangie Hurst  .̲͚͍̬̩͇͙̬̮̹͎͖̘̹͂̓̒̈́̾̈́̾͑́͋͋͋̓ͣ̀ <hangingwithhunter4@gmail.com>
2024-02-16 00:17:10 -0500

    fixed the paths for server.js

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// Set the static directory to 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'notes.html'));
-});
-
+// Serve index.html for all routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
 const PORT = process.env.PORT || 3005;
